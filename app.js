b0707b5a3f45fd276d50336975e6e7d723ccadd1
Yash Godwal <yashgodwal@gmail.com>
2024-04-03 22:38:38 +0530

    DELOY IT

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+const port=3222;
+// conenction to mongodb
+mongoose.connect("mongodb+srv://yashgodwal:porsche911@cluster0.k7ybm4v.mongodb.net/todo_express", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+
+// middlewares
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+
+app.listen(port,(req,res) => {
+    console.log(`SERVER IS LISTENING ON ${port}`);
+})
+// routes
+app.use(require("./routes/index"))
+app.use(require("./routes/todo"))
+
+
+// server configurations....
